13822b7c47108b419891b9212257c7c7dfddfe46
bradisoffline <46117390+bradisoffline@users.noreply.github.com>
2019-02-02 22:52:15 -0500

    Add files via upload

--- redirect.php
+++ redirect.php
@@ -1,14 +1,4 @@
 <?php
-
-$_POST['CODE'] = 'CODE'; // Simulate post data...
-$array_of_codes = array('CODE' => 'pathtoredirect');
-$get_code = addslashes(strtoupper($_POST['CODE']));
-
-if(array_key_exists($get_code, $array_of_codes))
-{
-    header("Location:".$array_of_codes[$get_code]."");
-}else
-{
-    echo "Error: Code not in array.";
-}
+    $link = "testLink";
+    header("Location: http://aaa.fr/".$link);
 ?>
